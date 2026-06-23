198cbc0b22a546c61d4fd6de64f18e41b81d5319
fabianonovo <fabianonogh1@gmail.com>
2026-06-23 20:22:36 -0300

    Algorithm_calculate_highest_value.py

---
+++ Algorithm_calculate_highest_value.py
@@ -0,0 +1,18 @@
+"""
+Algorithm: calculate which is the highest value
+Author: Novo
+Course: Conditional structures (Python)
+"""
+
+#input 
+a = float(input("Input the first value: "))
+b = float(input("Input the second value: "))
+
+#output
+if a>b:
+    print(f"The highest value is {a}")
+else:
+    print(f"The highest value is {b}")
+
+#end
+print ("end")
