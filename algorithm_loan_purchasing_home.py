9a5ad455b330b19d5d611efb06e800aab43a3b33
fabianonovo <fabianonogh1@gmail.com>
2026-06-26 12:24:49 -0300

    Fix validation order in loan approval algorithm

--- algorithm_loan_purchasing_home.py
+++ algorithm_loan_purchasing_home.py
@@ -11,10 +11,10 @@ years_to_pay = float(input("Enter how many years you plan to repay the house: ")
 monthly_payment = 0
 
 # process and output
-if salary * 1/3 < house_price / years_to_pay:
-    print("You are not eligible for that number of installments")
-elif house_price <= 0 or salary <= 0 or years_to_pay <= 0:
+if house_price <= 0 or salary <= 0 or years_to_pay <= 0:
     print("Please enter a valid value")
+elif salary * 1/3 < house_price / years_to_pay:
+    print("You are not eligible for that number of installments")
 else:
     months = years_to_pay * 12
     monthly_payment = house_price / months
