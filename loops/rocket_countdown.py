21941a4f925bdab855d683d231d221a9616a11db
fabianonovo <fabianonogh1@gmail.com>
2026-06-27 17:05:41 -0300

    fix: update rocket countdown to start at 11 and reflect it in header

---
+++ loops/rocket_countdown.py
@@ -0,0 +1,15 @@
+"""
+Algorithm: perform a rocket countdown from 11 to 0
+Author: Novo
+Course: Loop structures (Python)
+"""
+
+print("Rocket countdown from 11 to 0")
+
+# process and output
+a = 11
+while a != 0:
+    a = a - 1
+    print(a)
+if a == 0:
+    print("Launch!!")
