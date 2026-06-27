0527d58836068f6c1391c328485ddfa98ca8905a
fabianonovo <fabianonogh1@gmail.com>
2026-06-27 16:52:26 -0300

    feat: add loop example print_numbers_1_to_100 with english header

---
+++ loops/print_numbers_1_to_100.py
@@ -0,0 +1,13 @@
+"""
+Algorithm: print integer numbers from 1 to 100
+Author: Novo
+Course: Loop structures (Python)
+"""
+
+print("Algorithm to print the integer numbers from 1 to 100")
+
+# process and output
+a = 1
+while a < 100:
+    a = a + 1
+    print(a)
